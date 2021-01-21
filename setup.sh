22d27732097597a375efec0040ebf0603dd2110e
thailemall <58394712+thailemall@users.noreply.github.com>
2021-01-21 08:44:47 +0700

    update

--- setup.sh
+++ setup.sh
@@ -3,11 +3,14 @@
 docker stop mes
 docker stop gateway
 docker stop discovery
+docker stop auth
 docker rm mes
 docker rm gateway
 docker rm discovery
+docker rm auth
 docker rmi thailemall/mes:latest
 docker rmi thailemall/gateway:latest
 docker rmi thailemall/discovery:latest
+docker rim thailemall/jinnalee-mes:latest
 docker network create mes-network
 docker-compose up
